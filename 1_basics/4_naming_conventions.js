05dc89ff21759a8537866301813d8951023147f3
billihazero <billihazero@gmail.com>
2025-01-08 22:18:13 +0900

    feat: basic chapter

---
+++ 1_basics/4_naming_conventions.js
@@ -0,0 +1,23 @@
+/**
+ * Naming Conventions
+ * 
+ * 1) 일반적으로 영어를 사용
+ * 2) 특수기호는 언더스코어와 달러만
+ * 3) 숫자로 시작할 수 없어
+ * 4) 키워드는 변수명으로 사용할 수 없어
+ * 
+ */
+
+let factory = '코드';
+var $ive ='아이브';
+const _jeans = '뉴진스';
+
+console.log(factory);
+console.log($ive);
+console.log(_jeans);
+
+/**
+ * 1) camelCase - 대부분의 언어에서
+ * 2) snake_case - 파이썬같은 언어에서
+ * 3) PascalCase - C# 같은 언어에서 
+ */
